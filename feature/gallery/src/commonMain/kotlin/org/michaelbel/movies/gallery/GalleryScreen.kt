bed164e686ac3c6838b32ad440081456f86505cf
michaelbel <michael-bel@outlook.com>
2026-04-02 21:50:27 +0300

    Update project

--- feature/gallery/src/commonMain/kotlin/org/michaelbel/movies/gallery/GalleryScreen.kt
+++ feature/gallery/src/commonMain/kotlin/org/michaelbel/movies/gallery/GalleryScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -189,7 +190,8 @@ private fun GalleryScreenContent(
         modifier = Modifier.fillMaxSize(),
         snackbarHost = {
             SnackbarHost(
-                hostState = snackbarHostState
+                hostState = snackbarHostState,
+                modifier = Modifier.navigationBarsPadding()
             )
         },
         containerColor = MaterialTheme.colorScheme.primaryContainer,
